ebc02976441a49e820001695dadb8d7b40e087dc
sohaibahmed <sohaibahmed248@gmail.com>
2024-05-26 16:49:04 +0500

    update

---
+++ src/components/Certificate/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Certificate() {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <div className="w-4/5 border-8 rounded-full border-[#350C50] p-4 mt-4">
+          <h1 className="font-[500] text-[60px] text-[#350C50]">
+            Earn a career certificate
+          </h1>
+          <p className="font-[400] text-[20px] leading-[26px] text-[#000000] p-2">
+            Add this credential to your LinkedIn profile, resume, or CV Share it
+            on social media and in your performance review
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Certificate;
--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -4,12 +4,14 @@ import WhySection from "../../components/WhySection";
 import ProfessionalCertificateSec from "../../components/ProfessionalCertificateSec";
 import FrequentQuesSec from "../../components/FrequentQuesSec";
 import About from "../../components/About/About";
+import Certificate from "../../components/Certificate";
 
 function Course() {
   return (
     <>
       <CourseDeatilHeader />
       <About />
+      <Certificate />
       <ProfessionalCertificateSec />
       <WhySection />
 
