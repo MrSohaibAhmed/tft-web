14a7e07c8367c86431c04797cd342c154ea06f48
iman zulfiqar <imanzulfiqar298@gmail.com>
2024-05-28 00:49:03 +0500

    make footer mobile responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer">
-        <div className="main-footer-part">
+        <div className="main-footer-part gap-10 p-10 max-md:flex-wrap">
           <div
             className="footer-logo"
             data-aos="fade-up"
@@ -22,57 +22,58 @@ const Footer = () => {
           >
             <img src={Img} alt="Logo" />
           </div>
+          <div className="flex justify-between max-sm:flex-wrap gap-8 w-full">
+            <div
+              className={`footer-content ${activeIndex === 0 ? "active" : ""}`}
+              onClick={() => handleAccordionToggle(0)}
+              data-aos="fade-right"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+            >
+              <h3>Product</h3>
+              <p>Store</p>
+              <p>Developer</p>
+              <p>Teams</p>
+              <p>Pro</p>
+              <p>API Docs</p>
+              <p>Pricing</p>
+              <p>Changelog</p>
+            </div>
 
-          <div
-            className={`footer-content ${activeIndex === 0 ? "active" : ""}`}
-            onClick={() => handleAccordionToggle(0)}
-            data-aos="fade-right"
-            data-aos-offset="200"
-            data-aos-easing="ease-in-sine"
-            data-aos-duration="600"
-          >
-            <h3>Product</h3>
-            <p>Store</p>
-            <p>Developer</p>
-            <p>Teams</p>
-            <p>Pro</p>
-            <p>API Docs</p>
-            <p>Pricing</p>
-            <p>Changelog</p>
-          </div>
+            <div
+              className={`footer-content ${activeIndex === 1 ? "active" : ""}`}
+              onClick={() => handleAccordionToggle(1)}
+              data-aos="fade-down"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+            >
+              <h3>Company</h3>
+              <p>Careers</p>
+              <p>Manifesto</p>
+              <p>Privacy Policy</p>
+              <p>Terms of Services</p>
+              <p>DPA</p>
+              <p>Press Kit</p>
+              <p>Contact</p>
+            </div>
 
-          <div
-            className={`footer-content ${activeIndex === 1 ? "active" : ""}`}
-            onClick={() => handleAccordionToggle(1)}
-            data-aos="fade-down"
-            data-aos-offset="200"
-            data-aos-easing="ease-in-sine"
-            data-aos-duration="600"
-          >
-            <h3>Company</h3>
-            <p>Careers</p>
-            <p>Manifesto</p>
-            <p>Privacy Policy</p>
-            <p>Terms of Services</p>
-            <p>DPA</p>
-            <p>Press Kit</p>
-            <p>Contact</p>
-          </div>
-
-          <div
-            className={`footer-content ${activeIndex === 2 ? "active" : ""}`}
-            onClick={() => handleAccordionToggle(2)}
-            data-aos="fade-left"
-            data-aos-offset="200"
-            data-aos-easing="ease-in-sine"
-            data-aos-duration="600"
-          >
-            <h3>Community</h3>
-            <p>Community Stories</p>
-            <p>Slack</p>
-            <p>Twitter</p>
-            <p>Github</p>
-            <p>Dribble</p>
+            <div
+              className={`footer-content ${activeIndex === 2 ? "active" : ""}`}
+              onClick={() => handleAccordionToggle(2)}
+              data-aos="fade-left"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+            >
+              <h3>Community</h3>
+              <p>Community Stories</p>
+              <p>Slack</p>
+              <p>Twitter</p>
+              <p>Github</p>
+              <p>Dribble</p>
+            </div>
           </div>
         </div>
       </div>
